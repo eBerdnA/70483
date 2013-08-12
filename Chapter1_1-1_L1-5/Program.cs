e10fd579e7949ebc045a5837a3dba8326fbcf794
Andre Bering <andre@bering.in>
2013-08-12 22:35:18 +0200

    .

---
+++ Chapter1_1-1_L1-5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter1_1_1_L1_5
+{
+    class Program
+    {
+        //[ThreadStatic] // without this Attribute both threads access the same value
+        public static int _field;
+
+        public static void Main(string[] args)
+        {
+                    new Thread(() =>
+               { 
+                    for(int x = 0; x < 10; x++)
+                    {
+                        _field++;
+                        Console.WriteLine("Thread A: {0}", _field);
+                    }
+                }).Start();
+            new Thread(() =>
+                { 
+                    for(int x = 0; x < 10; x++)
+                    {
+                        _field++;
+                        Console.WriteLine("Thread B: {0}", _field);
+                    }
+                }).Start();
+            Console.ReadKey();
+        }
+    }        
+}
---
+++ Chapter1_1-1_L1-6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter1_1_1_L1_6
+{
+    class Program
+    {
+        public static ThreadLocal<int> _field =
+            new ThreadLocal<int>(() =>
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " " + Thread.CurrentThread.ManagedThreadId);
+                return Thread.CurrentThread.ManagedThreadId;
+            });
+
+        public static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine("Thread A: {0}", i);
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine("Thread B: {0}", i);
+                }
+            }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
